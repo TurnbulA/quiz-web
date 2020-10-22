52e11678cb399e3641aa7be3bd7df489d6dfe7e9
TurnbulA <turnbula@yw.co.uk>
2020-10-22 10:08:01 +0100

    Api fix

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module("app").factory("ConfigService", [
   function () {
     var service = {};
 
-    service.apiBase = "https://quiz-api-AT.eu-west-2.elasticbeanstalk.com/";
+    service.apiBase = "https://quiz-API-AT.eu-west-2.elasticbeanstalk.com/";
 
     return service;
   },
